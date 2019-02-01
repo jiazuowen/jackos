5e10675dface8cc97e7d98b34fd20e32d01762e9
Jack <jiazuowen@outlook.com>
2019-02-01 14:24:46 +0800

    第十六天修炼完成[F

--- jackos/bootpack.h
+++ jackos/bootpack.h
@@ -34,8 +34,9 @@ void farjmp(int eip, int cs);
 struct FIFO32 {
 	int *buf;
 	int p, q, size, free, flags;
+	struct TASK *task;
 };
-void fifo32_init(struct FIFO32 *fifo, int size, int *buf);
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task);
 int fifo32_put(struct FIFO32 *fifo, int data);
 int fifo32_get(struct FIFO32 *fifo);
 int fifo32_status(struct FIFO32 *fifo);
@@ -192,6 +193,8 @@ void inthandler20(int *esp);
 /*mtask.c*/
 #define MAX_TASKS		1000		/*最大的任务数量*/
 #define TASK_GDT0		3			/*从GDT的第几号开始分配给TSS*/
+#define MAX_TASKS_LV	100			/*每个level最多能创建多少个任务*/
+#define MAX_TASKLEVELS	10			/*一共有多少个level*/
 struct TSS32
 {
 	int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
@@ -200,18 +203,25 @@ struct TSS32
 	int ldtr, iomap;
 };
 struct TASK {
-	int sel, flags;		/*sel用来存放GDT的编号*/
+	int sel, flags;				/*sel用来存放GDT的编号*/
+	int level, priority;		/*任务优先级*/
 	struct TSS32 tss;
+};
+struct TASKLEVEL {
+	int running;				/*正在运行的任务数量*/
+	int now;					/*当前正在运行那个任务*/
+	struct TASK *tasks[MAX_TASKS_LV];
 };
 struct TASKCTL
 {
-	int running;				/*正在运行的任务数量*/
-	int now;					/*当前正在运行的是那个任务*/
-	struct TASK	*tasks[MAX_TASKS];
+	int now_lv;					/*现在活动中的level*/
+	char lv_change;				/*在下次任务切换时是否需要改变level*/
+	struct TASKLEVEL level[MAX_TASKLEVELS];
 	struct TASK	tasks0[MAX_TASKS];
 };
 extern struct TIMER	*task_timer;
 struct TASK	*task_init(struct MEMMAN *memman);
 struct TASK *task_alloc(void);
-void task_run(struct TASK *task);
+void task_run(struct TASK *task, int level, int priority);
 void task_switch(void);
+void task_sleep(struct TASK *task);

d97504b866df674d444903010cd8c50b23b0a327
Jack <jiazuowen@outlook.com>
2019-02-23 00:02:31 +0800

    添加API函数,malloc和free

--- jackos/console.c
+++ jackos/console.c
@@ -330,6 +330,24 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		sheet_slide(sht, 100, 50);
 		sheet_updown(sht, 3);	/*背景层3，位于task_a之上*/
 		reg[7] = (int) sht;
+	} else if (edx == 6) {
+		sht = (struct SHEET *) ebx;
+		putfonts8_asc(sht->buf, sht->bxsize, esi, edi, eax, (char *) ebp + ds_base);
+		sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+	} else if (edx == 7) {
+		sht = (struct SHEET *) ebx;
+		boxfill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
+		sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+	} else if (edx == 8) {
+		memman_init((struct MEMMAN *) (ebx + ds_base));
+		ecx &= 0xfffffff0;	/* 以十六字节为单位 */
+		memman_free((struct MEMMAN *) (ebx + ds_base), eax, ecx);
+	} else if (edx == 9) {
+		ecx = (ecx + 0x0f) & 0xfffffff0;
+		reg[7] = memman_alloc((struct MEMMAN *) (ebx + ds_base), ecx);
+	} else if (edx == 10) {
+		ecx = (ecx + 0x0f) & 0xfffffff0;
+		memman_free((struct MEMMAN *) (ebx + ds_base), eax, ecx);
 	}
 	return 0;
 }
